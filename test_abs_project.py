d799fccc68d03216a54e7131a23beec478dc8d86
Egor Solenok <solenok.egor@gmail.com>
2021-09-29 16:25:22 +0300

    Первый коммит в ветку

---
+++ test_abs_project.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of number"
+    
+if __name__ == "__main__":
+    test_abs1()
+    print("Al tests passed!")
