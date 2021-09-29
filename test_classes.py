4004a9a781634c5a8c83af33179aabfb1241556f
Egor Solenok <solenok.egor@gmail.com>
2021-09-29 18:49:37 +0300

    New another addition

---
+++ test_classes.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import unittest
+from selenium import webdriver
+import time
+
+class TestRegistration(unittest.TestCase):
+    def test_registration_one(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+            # Ваш код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector('.first_block [required].first')
+            input1.send_keys('One')
+            input2 = browser.find_element_by_css_selector('.first_block [required].second')
+            input2.send_keys('One')
+            input3 = browser.find_element_by_css_selector('.first_block [required].third')
+            input3.send_keys('One')
+                
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+            time.sleep(2)
+            
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+        
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be absolute value of a number")
+            
+        finally:
+            time.sleep(5)
+            browser.quit()
+
+    def test_registration_two(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+            # Ваш код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector('.first_block [required].first')
+            input1.send_keys('One')
+            input2 = browser.find_element_by_css_selector('.first_block [required].second')
+            input2.send_keys('One')
+            input3 = browser.find_element_by_css_selector('.first_block [required].third')
+            input3.send_keys('One')
+                
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+            time.sleep(2)
+            
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+        
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be absolute value of a number")
+            
+        finally:
+            time.sleep(5)
+            browser.quit()
+            
+                             
+if __name__ == "__main__":
+    unittest.main()
